a4e227182de21c77b3afb0115de7c68c793baf2b
Derek Townsend <derekjavon2@gmail.com>
2019-02-26 01:42:10 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -69,7 +69,7 @@ function sayHiToGrandma(string){
   if(string===string.toLowerCase()){
     return "I can't hear you!"
   }else if(string===string.toUpperCase()){
-    return "I can't hear you!"
+    return "YES INDEED!"
   }else if(string==="I love you, Grandma."){
     return "I can't hear you!"
   }
